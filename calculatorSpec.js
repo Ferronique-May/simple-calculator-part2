01a90e4615e4062bcf2eeca102cd822962c4ab57
Ferronique <ferronique.may@umuzi.org>
2019-05-31 16:44:10 +0200

    cleaned up calculatorSpec.js

--- calculatorSpec.js
+++ calculatorSpec.js
@@ -7,60 +7,43 @@ describe("Calculator",function(){
     beforeEach(function(){
         calculator = new Calculator();
     })
+
     it("should be able to add two numbers", function(){
-        var result = calculator.add(0,0);
-        expect(result).toBe(0);
-        
-        var result = calculator.add(4,5);
-        expect(result).toBe(9);
+        expect(calculator.add(0,0)).toBe(0);
+        expect(calculator.add(4,5)).toBe(9);
     });
    
     it("should be able to add negative integers", function(){
-        var result = calculator.add(-1,-1);
-        expect(result).toBe(-2);
-
-        var result = calculator.add(-1,2);
-        expect(result).toBe(1);
+        expect(calculator.add(-1,-1)).toBe(-2);
+        expect(calculator.add(-1,2)).toBe(1);
     })
 
     it("should be able to add multiple numbers", function(){
-        var result = calculator.add(1,2,3,4);
-        expect(result).toBe(10);
+        expect(calculator.add(1,2,3,4)).toBe(10);
     })
     it("should be able to add float numbers", function(){
-        var result = calculator.add(1.3,3.1,4.5,6.4);
-        expect(result).toBe(15.3);
+        expect(calculator.add(1.3,3.1,4.5,6.4)).toBe(15.3);
     })
     it("should be able to add numbers that are given as strings", function(){
-        var result = calculator.add(1,'2',3,'4',5);
-        expect(result).toBe(15);
+        expect(calculator.add(1,'2',3,'4',5)).toBe(15);
     })
 
     it("should be able to multiply two numbers", function(){
-        var result = calculator.multiply(1,2);
-        expect(result).toBe(2);
+        expect(calculator.multiply(1,2)).toBe(2);
     })
     it("should be able to multiply multiple numbers",function(){
-        var result = calculator.multiply(1,2,3,4);
-        expect(result).toBe(24);
-
-        var result = calculator.multiply(1,2,3,5);
-        expect(result).toBe(30);
+        expect(calculator.multiply(1,2,3,4)).toBe(24);
+        expect(calculator.multiply(1,2,3,5)).toBe(30);
     })
     it("should be able to multiply numbers that are given as strings",function(){
-        var result = calculator.multiply(1,'2',3,'5');
-        expect(result).toBe(30);
+        expect(calculator.multiply(1,'2',3,'5')).toBe(30);
     })
     it("should be able to multiply negative numbers", function(){
-        var result = calculator.multiply (-2,-1);
-        expect(result).toBe(2);
-
-        var result = calculator.multiply(-3,1);
-        expect(result).toBe(-3);
+        expect(calculator.multiply (-2,-1)).toBe(2);
+        expect(calculator.multiply(-3,1)).toBe(-3);
     })
     it("should be able to remember the last result", function(){
-        var result =calculator.add(1,2);
-        expect(result).toBe(3);
+        expect(calculator.add(1,2)).toBe(3);
         expect(calculator.last).toBe(3);
 
         expect(calculator.multiply(1,2,3,4)).toBe(24);
